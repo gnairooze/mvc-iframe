65b5e81e81b13fede97ab559209abc94ced61338
george.nairooze <george.nairooze@GEORGE-NAIROOZE>
2018-02-27 12:13:41 +0200

    add to git

---
+++ code/mvc-iframe/Controllers/ContactController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_iframe.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index(int? id)
+        {
+            Models.Contact contact = GetContact(id);
+
+            return View(contact);
+        }
+
+        public ActionResult Upload(int? id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload()
+        {
+            Models.Contact contact = GetContact(333);
+
+            for (int i = 0; i < Request.Files.Count; i++)
+            {
+                HttpPostedFileBase file = Request.Files[i]; //Uploaded file
+                                                            //Use the following properties to get file's name, size and MIMEType
+                int fileSize = file.ContentLength;
+                string fileName = file.FileName;
+                string mimeType = file.ContentType;
+                System.IO.Stream fileContent = file.InputStream;
+                //To save file, use SaveAs method
+                file.SaveAs(Server.MapPath("~/") + fileName); //File will be saved in application root
+                ViewBag.Message = "File uploaded";
+            }
+
+            return View();
+        }
+
+        private Models.Contact GetContact(int? age)
+        {
+            int defaultAge = 29;
+
+            return new Models.Contact()
+            {
+                Age = (age.HasValue)?age.Value:defaultAge,
+                Name = "T. Adel",
+                Phones = new List<string>() { "01222222", "01111111", "010000000", "0155555555" }
+            };
+        }
+    }
+}
---
+++ code/mvc-iframe/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc_iframe.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<string> Phones { get; set; }
+    }
+}
